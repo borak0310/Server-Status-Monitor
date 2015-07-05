386cd6f611f29d9af48546f9c173ea1a0546f540
borak0310 <borak0310>
2015-07-05 17:15:29 +0800

    view1.html - use angularJS ng-controller , ng-repeat  show server list info by json data

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,20 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
+    .controller('View1Ctrl', function ($scope) {
+        $scope.serverStatusList = [{
+            serverName: 'JBOSS AP1',
+            cpu: '50',
+            mem: '30',
+            conn: '10',
+            status: 'OK'
+        }, {serverName: 'JBOSS AP2', cpu: '50', mem: '30', conn: '10', status: 'OK'}];
 
-}]);
+    });
