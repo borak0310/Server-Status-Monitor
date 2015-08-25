6f9426d68c6192507fe333a8430eeece0a20a68e
Borak Chen <borak0310@gmail.com>
2015-08-25 20:49:00 +0800

    view2 modify scope detail

--- app/alert/alert.js
+++ app/alert/alert.js
@@ -10,10 +10,12 @@ angular.module('myApp.alert', ['ngRoute'])
     }])
 
     .controller('AlertCtrl', function ($scope) {
-        $scope.alertList = [{
-            serverIp: '172.17.120.aaa',
-            serverName: 'JBOSS AP1',
-            serverMsg:'記憶體不足'
-        }, {serverIp: '172.17.120.130', serverName: 'JBOSS AP2',serverMsg:'CPU LOAD過高'}];
+        $scope.alertList = [
+            {serverIp: '172.17.120.111',serverName: 'JBOSS AP1',serverMsg: '記憶體不足'},
+            {serverIp: '172.17.120.112', serverName: 'JBOSS AP2',serverMsg:'CPU LOAD過高'},
+            {serverIp: '172.17.120.113', serverName: 'JBOSS AP3',serverMsg:'網路連線數過多'},
+            {serverIp: '172.17.120.114', serverName: 'JBOSS AP5',serverMsg:'網路連線數過多'},
+            {serverIp: '172.17.120.115', serverName: 'JBOSS AP6',serverMsg:'記憶體不足'}
+        ];
 
     });
--- app/serverlist/serverlist.js
+++ app/serverlist/serverlist.js
@@ -14,9 +14,17 @@ angular.module('myApp.serverlist', ['ngRoute'])
     }])
 
     .controller('ServerListCtrl', function ($scope) {
-        $scope.serverList = [{
-            serverIp: '172.17.120.126',
-            serverName: 'JBOSS AP1'
-        }, {serverIp: '172.17.120.130', serverName: 'JBOSS AP2'}];
+        $scope.serverList = [
+            {serverIp: '172.17.120.126',serverName: 'JBOSS AP1'},
+            {serverIp: '172.17.120.130', serverName: 'JBOSS AP2'},
+            {serverIp: '172.17.120.111', serverName: 'JBOSS AP3'},
+            {serverIp: '172.17.120.112', serverName: 'JBOSS AP4'},
+            {serverIp: '172.17.120.113', serverName: 'JBOSS AP5'},
+            {serverIp: '172.17.120.114', serverName: 'JBOSS AP6'},
+            {serverIp: '172.17.120.115', serverName: 'JBOSS AP7'},
+            {serverIp: '172.17.120.116', serverName: 'JBOSS AP8'},
+            {serverIp: '172.17.120.117', serverName: 'JBOSS AP9'}
+        ];
+
 
     });
